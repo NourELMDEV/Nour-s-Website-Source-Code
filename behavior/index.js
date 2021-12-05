46bfd3edb7443ba18e085acbb77d369ce2a9a442
Nour/Elm <60050784+NourELMDEV@users.noreply.github.com>
2021-12-04 22:16:24 -0500

    Add files via upload

---
+++ behavior/index.js
@@ -0,0 +1,11 @@
+//auto type
+var typed = new Typed('.ii', {
+    strings: ["Print('Hello World')", ".innerHTML = 'Replaced'", "git push --force ^1000\n `pushed to origin with option force`", "git clone \n `https://github.com/RandomUser/Project.git`"],
+    typeSpeed:60,
+    backSpeed:20,
+    loop:true
+  });
+  //function onclick of a button
+  function helloworld(){
+      alert('hi')
+  }
